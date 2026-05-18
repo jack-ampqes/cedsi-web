5e605c116ff7e2e35a62da7bb3afb7c05ff83bee
jack <jack.lyons@ampqes.com>
2026-05-18 11:23:39 -0500

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,21 +20,11 @@ export const metadata: Metadata = {
   authors: [{ name: 'CEDSI' }],
   generator: 'v0.app',
   icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
+    icon: {
+      url: '/favicon.svg',
+      type: 'image/svg+xml',
+    },
+    apple: '/favicon.svg',
   },
 }
 
